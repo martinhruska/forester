3c02d2f8844bc2061ea6eee010cdd86e8dc7daca
Martin Hruska <hruskamartin25@gmail.com>
2015-10-30 18:04:24 +0100

    updated

--- fa/init_vata.sh
+++ fa/init_vata.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
+#cd libvata
+#git submodule init
+#git submodule update
+#make release
+
 cd libvata
-git submodule init
-git submodule update
+if [ ! -d build ]; then
+	mkdir build
+fi
 make release
